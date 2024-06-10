ccf3fa074f77ea45c0d6d52dff17d81c6add4a33
Pedro Andriotti <phab@cesar.school>
2024-06-09 21:58:56 -0300

    now admin can create a service

--- app/painel/clientes/[id]/page.tsx
+++ app/painel/clientes/[id]/page.tsx
@@ -1,22 +1,53 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import Cookies from 'js-cookie';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Card } from '@mui/material';
 import { Button } from '@/components/ui/button';
-import { Client, Service } from '@/lib/types';
 import MiscellaneousServicesIcon from '@mui/icons-material/MiscellaneousServices';
+import { Client, Service } from '@/lib/types';
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 const ClientDetailPage: React.FC = () => {
     const pathname = usePathname();
     const id = pathname.split('/').pop();
 
+    const router = useRouter();
+
     const [client, setClient] = useState<Client | null>(null);
     const [services, setServices] = useState<Service[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [type, setType] = useState('');
+    const [date, setDate] = useState('');
+    const [kilometers, setKilometers] = useState('');
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
 
     useEffect(() => {
         if (!id) return;
@@ -77,9 +108,53 @@ const ClientDetailPage: React.FC = () => {
             }
         };
 
+
         fetchClient();
         fetchServices();
     }, [id]);
+
+    const handleRegister = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const token = Cookies.get('accessToken');
+            if (!token) {
+                toast.error('Token not found');
+                return;
+            }
+
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/services`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    name,
+                    price: parseFloat(price),
+                    type,
+                    date: new Date(date).toISOString(),
+                    kilometers: parseInt(kilometers),
+                    userId: id,
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to register service');
+            }
+
+            toast.success('Serviço criado com sucesso!');
+            setName('');
+            setPrice('');
+            setType('');
+            setDate('');
+            setKilometers('');
+            setIsDialogOpen(false);
+
+        } catch (err) {
+            console.error('Error registering service:', err);
+            toast.error('Falha ao registrar o serviço.');
+        }
+    };
 
     if (loading) {
         return <div>Loading...</div>;
@@ -145,9 +220,97 @@ const ClientDetailPage: React.FC = () => {
                                 <span className='text-shineray-color-dark'><MiscellaneousServicesIcon /></span>
                                 <h2 className='text-2xl text-shineray-color-dark uppercase'>Serviços</h2>
                             </div>
-                            <Button className='bg-shineray-color-dark hover:bg-white hover:text-shineray-color-dark'>
-                                Cadastrar Serviço
-                            </Button>
+                            <div>
+                                <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+                                    <DialogTrigger asChild>
+                                        <Button className='bg-shineray-color-dark text-white hover:bg-white hover:text-shineray-color-dark'>Cadastrar Serviço</Button>
+                                    </DialogTrigger>
+                                    <DialogContent className="sm:max-w-[425px]">
+                                        <DialogHeader className='flex items-center'>
+                                            <DialogTitle>Cadastrar Serviço</DialogTitle>
+                                            <DialogDescription>
+                                                Insira as informações do Serviço
+                                            </DialogDescription>
+                                        </DialogHeader>
+
+                                        <form onSubmit={handleRegister} className="grid gap-4 py-4">
+                                            <div className="grid grid-cols-4 items-center gap-4">
+                                                <Label htmlFor="name" className="text-right">
+                                                    Nome
+                                                </Label>
+                                                <Input
+                                                    id="name"
+                                                    value={name}
+                                                    placeholder='Nome do Serviço'
+                                                    onChange={(e) => setName(e.target.value)}
+                                                    className="col-span-3"
+                                                    required
+                                                />
+                                            </div>
+                                            <div className="grid grid-cols-4 items-center gap-4">
+                                                <Label htmlFor="price" className="text-right">
+                                                    Preço
+                                                </Label>
+                                                <Input
+                                                    id="price"
+                                                    type="number"
+                                                    value={price}
+                                                    placeholder='R$ 0,00'
+                                                    onChange={(e) => setPrice(e.target.value)}
+                                                    className="col-span-3"
+                                                    required
+                                                />
+                                            </div>
+                                            <div className="grid grid-cols-4 items-center gap-4">
+                                                <Label htmlFor="type" className="text-right">
+                                                    Tipo
+                                                </Label>
+                                                <Select onValueChange={(value) => setType(value)} defaultValue={type}>
+                                                    <SelectTrigger className="col-span-3">
+                                                        <SelectValue placeholder="Selecione um Tipo" />
+                                                    </SelectTrigger>
+                                                    <SelectContent>
+                                                        <SelectGroup>
+                                                            <SelectLabel>Tipo</SelectLabel>
+                                                            <SelectItem value="GARANTIA">Garantia</SelectItem>
+                                                            <SelectItem value="PAGO">Pago</SelectItem>
+                                                        </SelectGroup>
+                                                    </SelectContent>
+                                                </Select>
+                                            </div>
+                                            <div className="grid grid-cols-4 items-center gap-4">
+                                                <Label htmlFor="date" className="text-right">
+                                                    Data
+                                                </Label>
+                                                <Input
+                                                    id="date"
+                                                    type="date"
+                                                    value={date}
+                                                    onChange={(e) => setDate(e.target.value)}
+                                                    className="col-span-3"
+                                                    required
+                                                />
+                                            </div>
+                                            <div className="grid grid-cols-4 items-center gap-4">
+                                                <Label htmlFor="kilometers" className="text-right">
+                                                    KM
+                                                </Label>
+                                                <Input
+                                                    id="kilometers"
+                                                    type="number"
+                                                    value={kilometers}
+                                                    onChange={(e) => setKilometers(e.target.value)}
+                                                    className="col-span-3"
+                                                    required
+                                                />
+                                            </div>
+                                            <DialogFooter>
+                                                <Button type="submit">Cadastrar</Button>
+                                            </DialogFooter>
+                                        </form>
+                                    </DialogContent>
+                                </Dialog>
+                            </div>
                         </div>
                         {services.length > 0 ? (
                             <ul className='list-disc ml-6'>
@@ -170,9 +333,8 @@ const ClientDetailPage: React.FC = () => {
                 </div>
             ) : (
                 <div className='text-red-500'>Cliente não encontrado</div>
-            )
-            }
-        </div >
+            )}
+        </div>
     );
 };
 
