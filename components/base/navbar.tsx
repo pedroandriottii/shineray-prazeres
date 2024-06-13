e3454905a918754572d79f942720a2c0239d9827
luismingati <luismingati@gmail.com>
2024-06-13 16:59:28 -0300

    navbar almost done

--- components/base/navbar.tsx
+++ components/base/navbar.tsx
@@ -1,37 +1,138 @@
 'use client';
 import React from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
-import Link from 'next/link';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
-
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from '../ui/sheet';
+import home from '@/public/home/home.svg'
+import motorcycle from '@/public/home/motorcycle.svg'
+import profile from '@/public/home/profile.svg'
+import folheto from '@/public/home/folheto.svg'
 const font = Inter({ subsets: ['latin'], weight: ['400'] });
 
 const Navbar: React.FC = () => {
     return (
         <div className={`${font.className} bg-shineray-black text-white flex justify-between text-xs items-center p-2`}>
-            <span className='md:hidden'>
-                <MenuIcon />
-            </span>
-            <div className='flex items-center gap-4'>
-                <div className='flex items-center gap-1'>
-                    <Image
-                        src={'/home/phone.svg'}
-                        alt='Telefone'
-                        width={20}
-                        height={20}
-                    />
-                    <span>(81) 98814-5906</span>
+            <Sheet >
+                <SheetTrigger className='md:hidden'><MenuIcon /></SheetTrigger>
+                <SheetContent side="left" className='bg-black border-black p-0 py-4'>
+                    <SheetHeader className='border-b-2 border-b-white w-full text-white h-10'>
+                    </SheetHeader>
+                    <div className='mt-6 px-5 flex flex-col gap-4'>
+                        <div className='flex text-white text-lg'>
+                            <a href="#" className='flex items-center gap-3'>
+                                <Image 
+                                    src={home}
+                                    width={25}
+                                    height={25}
+                                    alt='Home'
+                                />
+                                Início
+                            </a>
+                        </div>
+                        <div className='flex text-white text-lg'>
+                            <a href="#" className='flex items-center gap-3'>
+                                <Image 
+                                    src={motorcycle}
+                                    width={25}
+                                    height={25}
+                                    alt='sore nós'
+                                />
+                                Sobre nos
+                            </a>
+                        </div>
+                        <div className='flex text-white text-lg'>
+                            <a href="#" className='flex items-center gap-3'>
+                                <Image 
+                                    src={folheto}
+                                    width={25}
+                                    height={25}
+                                    alt='Catalogo'
+                                />
+                                Catalogo
+                            </a>
+                        </div>
+                        <div className='flex text-white text-lg'>
+                            <a href="#" className='flex items-center gap-3'>
+                                <Image 
+                                    src={profile}
+                                    width={25}
+                                    height={25}
+                                    alt='ver perfil'
+                                />
+                                Login
+                            </a>
+                        </div>
+                    </div>
+                </SheetContent>
+            </Sheet>
+            {/* DESKTOP */}
+            <div className='flex items-center justify-between max-md:justify-end w-full gap-4'>
+                <div className=' px-5 flex flex-col md:flex-row gap-4 max-md:hidden'>
+                    <div className='flex text-white text-lg'>
+                        <a href="#" className='flex items-center gap-3'>
+                            <Image 
+                                src={home}
+                                width={25}
+                                height={25}
+                                alt='Home'
+                            />
+                            Início
+                        </a>
+                    </div>
+                    <div className='flex text-white text-lg'>
+                        <a href="#" className='flex items-center gap-3'>
+                            <Image 
+                                src={motorcycle}
+                                width={25}
+                                height={25}
+                                alt='sore nós'
+                            />
+                            Sobre nos
+                        </a>
+                    </div>
+                    <div className='flex text-white text-lg'>
+                        <a href="#" className='flex items-center gap-3'>
+                            <Image 
+                                src={folheto}
+                                width={25}
+                                height={25}
+                                alt='Catalogo'
+                            />
+                            Catalogo
+                        </a>
+                    </div>
+                    <div className='flex text-white text-lg'>
+                        <a href="#" className='flex items-center gap-3'>
+                            <Image 
+                                src={profile}
+                                width={25}
+                                height={25}
+                                alt='ver perfil'
+                            />
+                            Login
+                        </a>
+                    </div>
                 </div>
-                <div className='flex items-center gap-1'>
-                    <Image
-                        src={'/home/instagram.svg'}
-                        alt='Instagram'
-                        width={35}
-                        height={35}
-                    />
-                    <span>@shinerayprazeres</span>
+                <div className=' flex'>
+                    <div className='flex items-center gap-1'>
+                        <Image
+                            src={'/home/phone.svg'}
+                            alt='Telefone'
+                            width={20}
+                            height={20}
+                        />
+                        <span>(81) 98814-5906</span>
+                    </div>
+                    <div className='flex items-center gap-1'>
+                        <Image
+                            src={'/home/instagram.svg'}
+                            alt='Instagram'
+                            width={35}
+                            height={35}
+                        />
+                        <span>@shinerayprazeres</span>
+                    </div>
                 </div>
             </div>
         </div>
--- components/home/Benefits.tsx
+++ components/home/Benefits.tsx
@@ -6,7 +6,7 @@ import capacete from '@/public/home/capacete.svg';
 import tools from '@/public/home/tools.svg';
 import { Inter, Poppins } from 'next/font/google';
 
-const FontInter = Inter({ subsets: ['latin'], weight: ['500'] });
+const FontInter = Inter({ subsets: ['latin'], weight: ['500', '700'] });
 const FontPoppins = Poppins({ subsets: ['latin'], weight: ['300'] });
 
 const Benefits: React.FC = () => {
@@ -15,7 +15,7 @@ const Benefits: React.FC = () => {
       <div className='bg-benefits bg-cover w-full bg-black h-full md:min-h-[600px] md:max-h-[600px] md:flex md:items-center md:justify-center'>
         <div className='md:justify-center md:items-center flex text-white max-w-[1700px] w-full'>
           <div className='flex md:my-auto flex-col flex-1 items-center justify-center py-10 md:py-14 gap-10 md:gap-20 '>
-            <h3 className={`${FontInter.className} text-lg text-center md:text-2xl uppercase underline underline-offset-4`}>Benefícios para nossos clientes</h3>
+            <h3 className={`${FontInter.className} text-lg text-center md:text-2xl uppercase underline underline-offset-4`}><span className='font-bold'>Benefícios</span> para nossos clientes</h3>
             <div className={`${FontPoppins.className} flex px-6 lg:px-12 flex-col gap-4 md:flex-row md:w-full md:justify-between`}>
               <div className='flex md:aspect-square md:flex-1 md:max-w-[250px] md:items-center md:justify-center md:flex-col md:text-center md:px-4 md:py-4 text-lg gap-2 md:gap-4 items-center border-2 px-3 py-3 rounded-lg bg-black/60'>
                 <Image src={threeStars} alt='Três estrelas' width={120} height={120} className='w-[45px] sm:w-[80px] md:w-[100px]' />
