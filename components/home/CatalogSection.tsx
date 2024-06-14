ee824baac1a14a7b0308b50cdc6512bacd246b1e
Pedro Andriotti <phab@cesar.school>
2024-06-14 19:55:34 -0300

    adding existing motorcycles on catalog section

--- components/home/CatalogSection.tsx
+++ components/home/CatalogSection.tsx
@@ -1,18 +1,47 @@
-import React from 'react';
-import { Poppins } from 'next/font/google';
-
-import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-} from "@/components/ui/carousel"
+'use client';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import { Motorcycle } from '@/lib/types';
+import { Poppins } from 'next/font/google';
+import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel"
 
 const Font = Poppins({ subsets: ['latin'], weight: ['400'] });
 
 const CatalogSection: React.FC = () => {
+    const [motorcycles, setMotorcycles] = useState<Motorcycle[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchMotorcycles = async () => {
+            try {
+                const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/motorcycles`, {});
+                if (!response.ok) {
+                    throw new Error('Failed to fetch motorcycles');
+                }
+                const data = await response.json();
+                setMotorcycles(data);
+            } catch (err) {
+                setError((err as Error).message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchMotorcycles();
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
     return (
-        <div className={`${Font.className}flex flex-col px-6 max-w-[1700px]`}>
+        <div className={`${Font.className} flex flex-col px-6 max-w-[1700px]`}>
             <div className='relative flex flex-1 z-0 justify-between items-center bg-black px-4 py-2 text-white'>
                 <h2 className='text-xl'>CATÁLOGO</h2>
                 <Link href={'/catalogo'}>
@@ -23,37 +52,20 @@ const CatalogSection: React.FC = () => {
             <div>
                 <Carousel className='py-6'>
                     <CarouselContent>
-                        <CarouselItem className="lg:basis-1/3 flex flex-col">
-                            <img src={'/jef.jpg'} alt="" />
-                            <div className='relative flex items-center flex-1 justify-between z-0 bg-gradient-to-r from-black to-[#797979] text-white p-2'>
-                                <p className=''>Urban 150 EFI</p>
-                                <p className='absolute right-0 p-2 bg-shineray-color-dark pl-6 max-w-28 min-w-28'>R$ 12.000</p>
-                                <div className="absolute right-28 top-0 border-corner-catalog"></div>
-                            </div>
-                        </CarouselItem>
-                        <CarouselItem className="lg:basis-1/3 flex flex-col">
-                            <img src={'/jef.jpg'} alt="" />
-                            <div className='relative flex items-center flex-1 justify-between z-0 bg-gradient-to-r from-black to-[#797979] text-white p-2'>
-                                <p className=''>Urban 150 EFI</p>
-                                <p className='absolute right-0 p-2 bg-shineray-color-dark pl-6 max-w-28 min-w-28'>R$ 12.000</p>
-                                <div className="absolute right-28 top-0 border-corner-catalog"></div>
-                            </div>
-                        </CarouselItem>
-                        <CarouselItem className="lg:basis-1/3 flex flex-col">
-                            <img src={'/jef.jpg'} alt="" />
-                            <div className='relative flex items-center flex-1 justify-between z-0 bg-gradient-to-r from-black to-[#797979] text-white p-2'>
-                                <p className=''>Urban 150 EFI</p>
-                                <p className='absolute right-0 p-2 bg-shineray-color-dark pl-6 max-w-28 min-w-28'>R$ 12.000</p>
-                                <div className="absolute right-28 top-0 border-corner-catalog"></div>
-                            </div>
-                        </CarouselItem>
-                        <CarouselItem className="lg:basis-1/3 flex flex-col">
-                            <img src={'/jef.jpg'} alt="" />
-                            <div className='relative flex items-center flex-1 justify-between z-0 bg-gradient-to-r from-black to-[#797979] text-white p-2'>
-                                <p className=''>Urban 150 EFI</p>
-                                <p className='absolute right-0 p-2 bg-shineray-color-dark pl-6 max-w-28 min-w-28'>R$ 12.000</p>
-                                <div className="absolute right-28 top-0 border-corner-catalog"></div>
-                            </div>
+                        {motorcycles.slice(0, 3).map((motorcycle) => (
+                            <CarouselItem key={motorcycle.id} className="lg:basis-1/3 flex flex-col">
+                                <img className='w-full' src={motorcycle.imageUrls[0]} alt={motorcycle.name} />
+                                <div className='relative flex items-center flex-1 justify-between z-0 bg-gradient-to-r from-black to-[#797979] text-white p-2'>
+                                    <p className=''>{motorcycle.name}</p>
+                                    <p className='absolute right-0 p-2 bg-shineray-color-dark pl-6 max-w-28 min-w-28'>R$ {motorcycle.price}</p>
+                                    <div className="absolute right-28 top-0 border-corner-catalog"></div>
+                                </div>
+                            </CarouselItem>
+                        ))}
+                        <CarouselItem className="lg:basis-1/3 flex flex-col relative bg-gradient-to-br from-black to-[#797979] items-center justify-center">
+                            <Link href={'/catalogo'}>
+                                <p className='underline text-white font-bold'>Visite nosso catálogo</p>
+                            </Link>
                         </CarouselItem>
                     </CarouselContent>
                 </Carousel>
