35d0a7ea8c35ebb76d2a4b6cd759fb1568e5419f
Pedro Andriotti <phab@cesar.school>
2024-06-19 22:20:16 -0300

    adding favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon16.png" />
+      </head>
       <Analytics />
       <SpeedInsights />
       <AuthProvider>
