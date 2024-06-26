4cd6c27f5adc91d9df2e28b60ba18ccd031624b3
Pedro Andriotti <phab@cesar.school>
2024-06-25 21:16:07 -0300

    fixing script link

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,10 +30,10 @@ export default function RootLayout({
         <Script id="google-tag-manager-head" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MPWK35S');
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MPWK358S');
           `}
         </Script>
         <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-16624479946" />
@@ -51,12 +51,11 @@ export default function RootLayout({
       <SpeedInsights />
       <AuthProvider>
         <body className={`${Font.className} font-light`}>
-          <Script id="google-tag-manager-body" strategy="afterInteractive">
-            {`
-              <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPWK35S"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-            `}
-          </Script>
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPWK358S"
+              height="0" width="0" className="hidden">
+            </iframe>
+          </noscript>
           <Navbar />
           {children}
           <a id="whatsapp-button" href={whatsappLink} target="_blank" rel="noopener noreferrer" className="fixed bottom-4 right-4 z-50 flex items-center justify-center w-16 h-16 bg-green-600 text-white rounded-full hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 shadow-lg">
